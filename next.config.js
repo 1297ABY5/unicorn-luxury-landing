e286489ba912f7be24cc90b5d361454edb5f2b73
1297ABY5 <1297aby5@gmail.com>
2025-06-15 20:58:36 +0400

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: { domains: ['your-cdn.com', 'oaidalleapiprodscus.blob.core.windows.net'] }
+};
+module.exports = nextConfig;
